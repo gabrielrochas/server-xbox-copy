fae949f43c86c86e77f388c90f1db50deea1a59f
Gabriel Rocha <gabriel.rocha@join4.com.br>
2021-10-08 16:42:31 -0300

    chore(game): imports `genres.module`

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from "./app.service";
 import { UserModule } from "./user/user.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { GamesModule } from "./games/games.module";
+import { GenresModule } from "./genres/genres.module";
 @Module({
-  imports: [UserModule, PrismaModule, GamesModule],
+  imports: [UserModule, PrismaModule, GamesModule, GenresModule],
   controllers: [AppController],
   providers: [AppService],
 })
